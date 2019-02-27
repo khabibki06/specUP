690548c9d59a4a415045cc0dc9e4f41cf43060a5
khabibki06 <khabibki06@gmail.com>
2019-02-27 16:15:24 +0700

    tes

--- file_io.R
+++ file_io.R
@@ -1,1 +1,8 @@
+#define S4 class called spectraUP
+setClass("spectraUP", slots=list(spectra = "data.frame", wavenumbers.label = "character", intensity.label = "character"))
 
+#function to read multi column csv file
+import.csv.long <- function(filename, wavenumbers.label = "wavenumbers", intensity.label = "intensity" ,sep = ",", dec = ".", header = TRUE) {
+  spectra_data <- read.csv(filename, header = header, sep = sep, dec = dec)
+  new("spectraUP", spectra = spectra_data, wavenumbers.label = wavenumbers.label, intensity.label = intensity.label)
+}
